a8a4a6b41801d91708b9ff39821204ed57c68b9a
zdwDavidzdw <163076975+zdwDavidzdw@users.noreply.github.com>
2026-04-01 13:10:37 +0800

    Update 1.5_agent.py

--- 1.5_agent.py
+++ 1.5_agent.py
@@ -104,7 +104,6 @@ To use a tool, please use the following format:
 
 ```
 Thought: Do I need to use a tool? Yes
-Action: 文档检索
 Action: the action to take, should be one of [{tool_names}]
 Action Input: {input}
 Observation: the result of the action
@@ -115,7 +114,7 @@ When you have a response to say to the Human, or if you do not need to use a too
 ```
 Thought: Do I need to use a tool? No
 Final Answer: [your response here]
-```
+‍```
 
 Begin!
 

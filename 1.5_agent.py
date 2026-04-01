e008ed00fea834267f35234c55a9a5a5a43258a9
zdwDavidzdw <163076975+zdwDavidzdw@users.noreply.github.com>
2026-04-01 11:20:47 +0800

    Update 1.5_agent.py

--- 1.5_agent.py
+++ 1.5_agent.py
@@ -1,8 +1,4 @@
 import streamlit as st
-# 强制禁用前端缓存，解决DOM冲突
-st.set_option('server.enableCORS', False)
-st.set_option('server.enableXsrfProtection', False)
-
 import tempfile
 import os
 #适配 langchain 0.3.2 新路径
